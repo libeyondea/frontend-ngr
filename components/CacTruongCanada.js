56cce82c581c94477334a77ffaa659d545f87445
eRis1992 <59914384+eRis1992@users.noreply.github.com>
2022-04-12 11:00:51 +0700

    up

--- components/CacTruongCanada.js
+++ components/CacTruongCanada.js
@@ -2,20 +2,22 @@ import React, { useState } from 'react';
 import Swiper from 'react-id-swiper';
 import Image from '../components/Image';
 const CacTruongCanada = () => {
-return (
-    <div>
-        <div class="page-head">
-		
-        <h1 class="page-title">
-            Định Cư Mỹ			</h1>
-        
-                    <a class="rss-cat-icon ttip" href="https://newgoldenroad.com/chuyen-muc/dinh-cu/dinh-cu-my/feed/" original-title="Feed Subscription"><i class="fa fa-rss"></i></a>
-                    
-        <div class="stripe-line"></div>
+	return (
+		<div>
+			<div className="page-head">
+				<h1 className="page-title">Định Cư Mỹ </h1>
 
-                </div>
+				<a
+					className="rss-cat-icon ttip"
+					href="https://newgoldenroad.com/chuyen-muc/dinh-cu/dinh-cu-my/feed/"
+					original-title="Feed Subscription"
+				>
+					<i className="fa fa-rss"></i>
+				</a>
 
-    </div>
+				<div className="stripe-line"></div>
+			</div>
+		</div>
 	);
 };
 export default CacTruongCanada;
