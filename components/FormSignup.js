d55938bc9b219faa39719b6b5f5c5654f589d02e
TramJulie3012 <103029978+TramJulie3012@users.noreply.github.com>
2022-04-13 16:49:36 +0700

    update

--- components/FormSignup.js
+++ components/FormSignup.js
@@ -6,9 +6,31 @@ const FormSignup = () => {
 		<section className="countdown-one">
 			<div className="container">
 				<div className="row">
-					<div className="col-lg-6">
+				<div className="col-lg-4">
+						<div className="countdown-one__content">
+							<h2 className="countdown-one__title">TẠI SAO PHẢI CHỌN TÂN CON ĐƯỜNG VÀNG ? </h2>
+							<p className="countdown-one__tag-line">Du học tân con đường vàng nơi chấp cánh ước mơ</p>
+							<p className="countdown-one__text">
+								Là một công ty tư vấn du học hàng đầu ở Việt Nam, được các lãnh sự Mỹ, Úc, Canada tin cậy, là đại
+								diện chính thức cho các trường trong hệ thống giáo dục của Mỹ, Anh, Úc, Canada, New Zealand,
+								Singapore….
+								<br />								
+
+								Đội ngũ nhân viên của New Golden Road đã từng làm việc nhiều năm trong môi trường giáo dục, với bề
+								dày kinh nghiệm và kiến thức chuyên môn vững chắc sẽ góp tay chung sức chấp cánh cho ước mơ của
+								các em học sinh – sinh viên Việt Nam bay cao hơn, đạt được khát vọng cũng như thành công mỹ mãn
+								trên con đường sự nghiệp của mình và còn là các nhân tài cho đất nước hôm nay và mai sau.
+								<br />
+								New GOLDEN ROAD luôn đồng hành cùng du học sinh trong suốt chặn đường học tập tại các nước trên
+								thế giới, đồng thời phối hợp cùng nhà trường, và các tổ chức giáo dục trên thế giới để hổ trợ các
+								vấn đề khó khăn mà du học sinh gặp phải.
+							</p>
+						</div>
+					</div>
+					<div className="col-lg-2">
 						<div className="countdown-one__content">
 							<h2 className="countdown-one__title">TỶ GIÁ NGOẠI TỆ</h2>
+
 							<table class='table-ngoaite'> 				
 								<tr>
 									<th>Mã Ngoại Tệ</th>
