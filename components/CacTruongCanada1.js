8ad887dd9590a68b739c4d41d0d1039c666fda91
TramJulie3012 <103029978+TramJulie3012@users.noreply.github.com>
2022-04-13 16:02:24 +0700

    update

--- components/CacTruongCanada1.js
+++ components/CacTruongCanada1.js
@@ -17,62 +17,62 @@ const CacTruongCanada1 = () => {
 				<h2 class="inner-banner-THCA__title">Các Trường Đại Học <br/>Và Cao Đẳng Canada</h2>
 			</div>    
 		</section>    
-		<div>
-			<div class="card">
+		<div class="Main">
+			<div class="card-TH">
 				<div class="card-header">
 					<p class="card-header-TH">Trường Southern Alberta Institute of Technology (SAIT) – Du học Canada – Du học Tân Con Đường Vàng</p>
 				</div>
 				<div class="card-body">
-					<p class='img-card'>
+					<p class='img-card-TH'>
 						<img src="/assets/images/TruongHoc/SAIT.png" alt="Trường Southern Alberta Institute of Technology (SAIT)" />
 					</p>
 					<h5 class="card-title-TH">Trường Southern Alberta Institute of Technology (SAIT)</h5>
-					<p class="card-text">SASKATCHEWAN POLYTECHNIC  Saskatchewan Polytechnic được thành lập vào năm 1941 tọa lạc tại Saskatchewan –
+					<p class="card-text-TH">SASKATCHEWAN POLYTECHNIC  Saskatchewan Polytechnic được thành lập vào năm 1941 tọa lạc tại Saskatchewan –
 					nơi được ví von là “Land of Living Skies” của Canada.  Cùng với 12 cơ sở nghiên cứu chuyên sâu, Saskatchewan Polytechnic còn 
 					là thành viên của Nhóm các trường bách khoa Canada (Polytechnics Canada) …</p>
 					<a href="#" class="btn btn-primary">Đọc Thêm</a>
 				</div>
 			</div>
-			<div class="card">
+			<div class="card-TH">
 				<div class="card-header">
 					<p class="card-header-TH">Trường Saskatchewan Polytechnic – Du học Canada – Du học Tân Con Đường Vàng</p>
 				</div>
 				<div class="card-body">
-					<p class='img-card'>
+					<p class='img-card-TH'>
 						<img src="/assets/images/TruongHoc/SASK.png" alt="Trường Saskatchewan Polytechnic" />
 					</p>
 					<h5 class="card-title-TH">Trường Saskatchewan Polytechnic</h5>
-					<p class="card-text">SASKATCHEWAN POLYTECHNIC  Saskatchewan Polytechnic được thành lập vào năm 1941 tọa lạc tại Saskatchewan – 
+					<p class="card-text-TH">SASKATCHEWAN POLYTECHNIC  Saskatchewan Polytechnic được thành lập vào năm 1941 tọa lạc tại Saskatchewan – 
 					nơi được ví von là “Land of Living Skies” của Canada.  Cùng với 12 cơ sở nghiên cứu chuyên sâu, Saskatchewan Polytechnic còn 
 					là thành viên của Nhóm các trường bách khoa Canada (Polytechnics Canada) …</p>
 					<a href="#" class="btn btn-primary">Đọc Thêm</a>
 				</div>
 			</div>
-			<div class="card">
+			<div class="card-TH">
 				<div class="card-header">
 					<p class="card-header-TH">Trường British Columbia University – Du học Canada – Du học Tân Con Đường Vàng</p>
 				</div>
 				<div class="card-body">
-					<p class='img-card'>
+					<p class='img-card-TH'>
 						<img src="/assets/images/TruongHoc/BRITI.png" alt="Trường British Columbia University" />
 					</p>
 					<h5 class="card-title-TH">Trường British Columbia University</h5>
-					<p class="card-text">BRITISH COLUMBIA UNIVERSITY British Columbia – ngôi trường của những giải Nobel và những người nổi tiếng. 
+					<p class="card-text-TH">BRITISH COLUMBIA UNIVERSITY British Columbia – ngôi trường của những giải Nobel và những người nổi tiếng. 
 					Nếu Mỹ có Đại học Havard, Anh có Đại học Oxford thì Canada có Đại học British Columbia. Du học, ai cũng mong được sở hữu tấm bằng 
 					của những trường danh tiếng. …</p>
 					<a href="#" class="btn btn-primary">Đọc Thêm</a>
 				</div>
 			</div>
-			<div class="card">
+			<div class="card-TH">
 				<div class="card-header">
 					<p class="card-header-TH">Trường Coast Mountain College – Du học Canada – Du học Tân Con Đường Vàng</p>
 				</div>
 				<div class="card-body">
-					<p class='img-card'>
+					<p class='img-card-TH'>
 							<img src="/assets/images/TruongHoc/COAST.png" alt="Trường Coast Mountain College" />
 					</p>
 					<h5 class="card-title-TH">Trường Coast Mountain College</h5>
-					<p class="card-text">COAST MOUNTAIN COLLEGE Coast Mountain College (CMTN) được thành lập dưới sự công nhận của Chính phủ. 
+					<p class="card-text-TH">COAST MOUNTAIN COLLEGE Coast Mountain College (CMTN) được thành lập dưới sự công nhận của Chính phủ. 
 					Toạ lạc tại khu vực cộng đồng dân cư thịnh vượng và đa dạng của Tây Bắc thuộc tỉnh British Columbia. Coast Mountain
 						College cung cấp nhiều chương trình đào tạo chất lượng với …</p>
 					<a href="#" class="btn btn-primary">Đọc Thêm</a>
