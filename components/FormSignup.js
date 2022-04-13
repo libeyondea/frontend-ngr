1080743ba93f6570b8016cd6c329b429c8c163f5
TramJulie3012 <103029978+TramJulie3012@users.noreply.github.com>
2022-04-13 09:06:56 +0700

    update

--- components/FormSignup.js
+++ components/FormSignup.js
@@ -8,10 +8,8 @@ const FormSignup = () => {
 				<div className="row">
 					<div className="col-lg-6">
 						<div className="countdown-one__content">
-							<h2 className="countdown-one__title">TẠI SAO PHẢI CHỌN TÂN CON ĐƯỜNG VÀNG ? </h2>
-							<p className="countdown-one__tag-line">Du học tân con đường vàng nơi chấp cánh ước mơ</p>
-							<table className='table-ngoaite'> 
-								<tr>TỶ GIÁ NGOẠI TỆ</tr>
+							<h2  className="countdown-one__title">TỶ GIÁ NGOẠI TỆ</h2>
+							<table class='table-ngoaite'> 				
 								<tr>
 									<th>Mã NT</th>
 									<th>Mua Vào</th>
