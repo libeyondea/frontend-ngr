b2c26f67b94122893c10ce79025f53b1102fe5c0
TramJulie3012 <103029978+TramJulie3012@users.noreply.github.com>
2022-04-14 15:51:12 +0700

    pull

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Script from 'next/script';
+import Chat from './Chat';
 
 const Layout = (props) => {
 	return (
@@ -8,6 +9,7 @@ const Layout = (props) => {
 				<title>{props.pageTitle}</title>
 			</Head>
 			<div className="page-wrapper">{props.children}</div>
+			<Chat />
 			<Script src="/assets/plugins/bootstrap/jquery.min.js" strategy="beforeInteractive" />
 			<Script src="/assets/plugins/bootstrap/bootstrap.min.js" strategy="beforeInteractive" />
 			<Script src="https://sp.zalo.me/plugins/sdk.js" strategy="afterInteractive" />
