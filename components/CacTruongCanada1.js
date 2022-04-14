3ed9d4d322f7034f7327a2392ea3e1742f24f2fc
TramJulie3012 <103029978+TramJulie3012@users.noreply.github.com>
2022-04-14 15:15:22 +0700

    update

--- components/CacTruongCanada1.js
+++ components/CacTruongCanada1.js
@@ -84,23 +84,6 @@ const CacTruongCanada1 = () => {
       </div>
     </div>
   </div>
-  <div className="col-lg-5">
-						<div className="become-teacher__form" id="back-top">
-							<div className="become-teacher__form-top">
-								<h2 className="become-teacher__form-title">TƯ VẤN MIỄN PHÍ</h2>
-							</div>
-							<form action="#" metdod="POST" className="become-teacher__form-content contact-form-validated">
-								<input type="text" placeholder="Họ và Tên" name="name" />
-								<input type="text" placeholder=" Địa chỉ Email" name="email" />
-								<input type="text" placeholder="Số điện tdoại" name="phone" />
-								<input type="text" placeholder="Lời nhắn" name="message" />
-								<button type="submit" className="tdm-btn become-teacher__form-btn">
-									ĐĂNG KÝ
-								</button>
-							</form>
-							<div className="result text-center"></div>
-						</div>
-					</div>
   <div className="post-pagination">
     <a href="#">
       <i className="fa fa-angle-double-left" />
