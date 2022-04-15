920792bcd27f7f29e1abbff3c1ad9128c4005823
Anh Đây Mấy Đứa <103025377+letuananh99@users.noreply.github.com>
2022-04-15 16:46:28 +0700

    up

---
+++ components/DinhCuUc.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const DinhCuUc = () => {
+	return (
+		<section className="team-one team-page">
+
+		</section>
+	);
+};
+
+export default DinhCuUc;
--- pages/dinhcuuc.js
+++ pages/dinhcuuc.js
@@ -3,16 +3,14 @@ import Layout from '../components/Layout';
 import NavOne from '../components/NavOne';
 import PageHeader from '../components/PageHeader';
 import Footer from '../components/Footer';
-import CallToActionTwo from '../components/CallToActionTwo';
-import Doitac from '../components/Doitac';
+import DinhCuUc from '../components/DinhCuUc';
 
 const TeachersPage = () => {
 	return (
 		<Layout pageTitle="Tân Con Đường Vàng | Định Cư">
 			<NavOne />
 			<PageHeader title="Định Cư Úc" />
-			<Doitac />
-			<CallToActionTwo />
+			<DinhCuUc />
 			<Footer />
 		</Layout>
 	);
