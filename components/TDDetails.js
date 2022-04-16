10b8a150a33037f6c46036ddfccf560bb48ecab4
hieuhunter <84726273+hieuhunter@users.noreply.github.com>
2022-04-16 15:35:40 +0700

    up_td2
    2

--- components/TDDetails.js
+++ components/TDDetails.js
@@ -178,7 +178,16 @@ const TuyenDungDetails = () => {
                                     <br />
                                     <p className="auto"><strong><span >DU HỌC</span>&nbsp;<span>TÂN CON ĐƯỜNG VÀNG – NEW GOLDEN ROAD</span></strong></p>
 
-                                    <br />
+    
+                                    <p dir="auto" style={{ textAlign: 'center' }}>Website:&nbsp;<a href="https://newgoldenroad.com/">https://newgoldenroad.com/</a></p>
+                                    <p dir="auto" style={{ textAlign: 'center' }}>Địa chỉ: 219 Tân Quý, phường Tân Quý, quận Tân Phú, Tp Hồ Chí Minh</p>
+
+                                    <p dir="auto" style={{ textAlign: 'center' }}>Email: duhoc@newgoldenroad.edu.vn</p>
+
+                                    <p dir="auto" style={{ textAlign: 'center' }}>Facebook:&nbsp;<a href="https://www.facebook.com/newgoldenroad">https://www.facebook.com/newgoldenroad</a></p>
+
+                                    <br />                             
+
                                     <ul className="course-details__curriculum-list list-unstyled">
                                         <li>
                                             <div className="course-details__curriculum-list-left">
@@ -194,71 +203,39 @@ const TuyenDungDetails = () => {
                                         <li>
                                             <div className="course-details__curriculum-list-left">
                                                 <div className="course-details__meta-icon video-icon icon-gg">
-                                                    <svg  xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-google" viewBox="0 0 16 16" style={{ color: 'white' }}>
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-google" viewBox="0 0 16 16" style={{ color: 'white' }}>
                                                         <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z" />
                                                     </svg>
                                                 </div>
-                                                <a  href="#">duhoc@newgoldenroad.edu.vn</a> <span>Preview</span>
+                                                <a href="#">duhoc@newgoldenroad.edu.vn</a> <span>Preview</span>
                                             </div>
                                             <div className="course-details__curriculum-list-right">10 minutes</div>
                                         </li>
                                         <li>
                                             <div className="course-details__curriculum-list-left">
                                                 <div className="course-details__meta-icon file-icon">
-                                                    <i className="fas fa-folder"></i>
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-instagram" viewBox="0 0 16 16" style={{ color: 'white' }}>
+                                                        <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z" />
+                                                    </svg>
                                                 </div>
-                                                <a href="#">Basic Editing Technology</a>
+                                                <a href="#">instagram</a>
                                             </div>
                                         </li>
                                         <li>
                                             <div className="course-details__curriculum-list-left">
-                                                <div className="course-details__meta-icon quiz-icon">
-                                                    <i className="fas fa-comment"></i>
+                                                <div className="course-details__meta-icon quiz-icon icon-gg">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pinterest" viewBox="0 0 16 16" style={{ color: 'white' }}>
+                                                        <path d="M8 0a8 8 0 0 0-2.915 15.452c-.07-.633-.134-1.606.027-2.297.146-.625.938-3.977.938-3.977s-.239-.479-.239-1.187c0-1.113.645-1.943 1.448-1.943.682 0 1.012.512 1.012 1.127 0 .686-.437 1.712-.663 2.663-.188.796.4 1.446 1.185 1.446 1.422 0 2.515-1.5 2.515-3.664 0-1.915-1.377-3.254-3.342-3.254-2.276 0-3.612 1.707-3.612 3.471 0 .688.265 1.425.595 1.826a.24.24 0 0 1 .056.23c-.061.252-.196.796-.222.907-.035.146-.116.177-.268.107-1-.465-1.624-1.926-1.624-3.1 0-2.523 1.834-4.84 5.286-4.84 2.775 0 4.932 1.977 4.932 4.62 0 2.757-1.739 4.976-4.151 4.976-.811 0-1.573-.421-1.834-.919l-.498 1.902c-.181.695-.669 1.566-.995 2.097A8 8 0 1 0 8 0z" />
+                                                    </svg>
                                                 </div>
-                                                <a href="#">Quiz</a>
+                                                <a href="#">pinterest</a>
                                             </div>
                                             <div className="course-details__curriculum-list-right">6 questions</div>
                                         </li>
                                     </ul>
                                     <br />
                                     <br />
-                                    <h3 className="course-details__tab-title">Intermediate Level</h3>
-                                    <br />
-                                    <p className="course-details__tab-text">
-                                        Aelltes port lacus quis enim var sed efficitur turpis gilla sed sit Lorem Ipsum is simply
-                                        dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s
-                                        standard dummy text ever since.
-                                    </p>
-                                    <br />
-                                    <ul className="course-details__curriculum-list list-unstyled">
-                                        <li>
-                                            <div className="course-details__curriculum-list-left">
-                                                <div className="course-details__meta-icon video-icon">
-                                                    <i className="fas fa-play"></i>
-                                                </div>
-                                                <a href="#">Introduction to Editing</a>
-                                                <span>Preview</span>
-                                            </div>
-                                            <div className="course-details__curriculum-list-right">16 minutes</div>
-                                        </li>
-                                        <li>
-                                            <div className="course-details__curriculum-list-left">
-                                                <div className="course-details__meta-icon file-icon">
-                                                    <i className="fas fa-folder"></i>
-                                                </div>
-                                                <a href="#">Basic Editing Technology</a>
-                                            </div>
-                                        </li>
-                                        <li>
-                                            <div className="course-details__curriculum-list-left">
-                                                <div className="course-details__meta-icon quiz-icon">
-                                                    <i className="fas fa-comment"></i>
-                                                </div>
-                                                <a href="#">Quiz</a>
-                                            </div>
-                                            <div className="course-details__curriculum-list-right">6 questions</div>
-                                        </li>
-                                    </ul>
+
                                 </div>
                                 <div className="tab-pane  animated fadeInUp" role="tabpanel" id="review">
                                     <div className="row">
