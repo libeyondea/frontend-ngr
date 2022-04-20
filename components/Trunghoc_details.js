0e2ddf8f0bd3942cd9087440dc4a790ec92138c8
pt1312 <trangnguyen13122000@gmail.com>
2022-04-20 15:22:11 +0700

    upth

--- components/Trunghoc_details.js
+++ components/Trunghoc_details.js
@@ -50,12 +50,12 @@ const Trunghoc_details = () => {
 				</div>
 			
 			<div className="row">
-				
+		
 				<div className="col-lg-8">
-				<div>
+					<div className="noidung">
 				<h1 className="tieudeTH">BODWELL HIGH SCHOOL – Vancouver, British Columbia– Du học Tân Con Đường Vàng</h1>
-				<div className="noidung1">
-					{' '}
+				
+				
 					<div style={{ color: '#333', fontSize: '20px', fontWeight: 'lighter' }}>
 						<strong>
 							<b>Bodwell High School </b>
@@ -71,18 +71,18 @@ const Trunghoc_details = () => {
 							className="anh1"
 						/>
 					</div>
-				</div>
-				<div>
+
+
 					<h2 className="mucTH">CHƯƠNG TRÌNH ĐÀO TẠO</h2>
 					<div style={{ color: '#333', fontSize: '20px', fontWeight: 'lighter' }}> Cử nhân thương mại
 					Chương trình học của trường Bodwell High School được kiểm tra cẩn thận, có ý nghĩa, đa dạng,
 					 cân bằng và quan trọng nhất là tăng cường sự phát triển hài hòa về nhân cách, trí tuệ và thể chất. Trường Bodwell
 					  không ngừng đảm bảo sự cải thiện và điều chỉnh chương trình học theo các tiêu chuẩn của Sở Giáo Dục British Columbia.
 					  <ul >
-							<li className="list"> Bậc đào tạo: lớp 8-12</li>
-							<li className="list"> Chương trình dự bị đại học</li>
-							<li className="list"> Tiếng anh học thuật và dự bị</li>
-							<li className="list"> Khóa học hè</li>
+							<li > &#10004; Bậc đào tạo: lớp 8-12</li>
+							<li > &#10004; Chương trình dự bị đại học</li>
+							<li > &#10004; Tiếng anh học thuật và dự bị</li>
+							<li > &#10004; Khóa học hè</li>
 							</ul>
 					</div>
 					<div className="anh-noidung1">
@@ -92,15 +92,14 @@ const Trunghoc_details = () => {
 							className="anh1"
 						/>
 					</div>
-				</div>
 				<div>
 					<h2 className="mucTH">ĐIỂM NỔI BẬT CỦA BODWELL HIGH SCHOOL</h2>
 					<div style={{ color: '#333', fontSize: '20px', fontWeight: 'lighter' }}>
 					<ul >
-							<li className="list"> Cơ sở vật chất hiện đại với một hồ bơi trong nhà, 2 phòng tập, 2 giảng đường, 2 căn tin, các phòng thí nghiệm khoa học, một thư viện và một phòng nhạc.</li>
-							<li className="list"> Tỷ lệ học sinh đậu tốt nghiệp hằng năm là 97% và được ghi danh vào các trường Đại học danh tiếng ở Canada.</li>
-							<li className="list"> Có đội ngũ tư vấn giúp đỡ cho du học sinh khi gặp vấn đề khó khăn và hướng dẫn chọn trường đại học phù hợp với sở thích của từng du học sinh.</li>
-							<li className="list"> Bodwell cấp cho tất cả học sinh laptop được tối ưu cho nghiên cứu học tập, gồm có phần mềm thiết yếu như Microsoft Office 365, Adobe Creative Cloud.</li>
+							<li > &#10004; Cơ sở vật chất hiện đại với một hồ bơi trong nhà, 2 phòng tập, 2 giảng đường, 2 căn tin, các phòng thí nghiệm khoa học, một thư viện và một phòng nhạc.</li>
+							<li > &#10004; Tỷ lệ học sinh đậu tốt nghiệp hằng năm là 97% và được ghi danh vào các trường Đại học danh tiếng ở Canada.</li>
+							<li > &#10004; Có đội ngũ tư vấn giúp đỡ cho du học sinh khi gặp vấn đề khó khăn và hướng dẫn chọn trường đại học phù hợp với sở thích của từng du học sinh.</li>
+							<li > &#10004; Bodwell cấp cho tất cả học sinh laptop được tối ưu cho nghiên cứu học tập, gồm có phần mềm thiết yếu như Microsoft Office 365, Adobe Creative Cloud.</li>
 							</ul>
 							</div>
 							<div className="anh-noidung1">
@@ -116,9 +115,9 @@ const Trunghoc_details = () => {
 					<div style={{ color: '#333', fontSize: '20px', fontWeight: 'lighter' }}>
 					Ngoài việc học tập, Bodwell cũng chú trọng vào tinh thần và thể chất của học sinh. Trường thường tổ chức nhiều sự kiện văn hóa, giải trí:
 					<ul >
-							<li className="list"> Các chuyến tham quan rạp hát</li>
-							<li className="list"> Các cuộc thi đấu thể thao</li>
-							<li className="list"> Các chuyến dã ngoại đến những thành phố khác nhau ở Canada và tất cả học sinh đều phải tham gia.</li>
+							<li > &#10004; Các chuyến tham quan rạp hát</li>
+							<li > &#10004; Các cuộc thi đấu thể thao</li>
+							<li > &#10004; Các chuyến dã ngoại đến những thành phố khác nhau ở Canada và tất cả học sinh đều phải tham gia.</li>
 							</ul>
 							</div>
 							<div className="anh-noidung1">
@@ -146,18 +145,19 @@ const Trunghoc_details = () => {
 						/>
 					</div>
 					</div>
-				</div>
 				<div>
 				<h2 className="mucTH">CHI PHÍ</h2>
 					<div style={{ color: '#333', fontSize: '20px', fontWeight: 'lighter' }}>
 					<ul >
-							<li className="list"> Học phí: $18,000/năm</li>
-							<li className="list"> Chi phí ăn ở: $16,000/năm</li>
+							<li > &#10004; Học phí: $18,000/năm</li>
+							<li > &#10004; Chi phí ăn ở: $16,000/năm</li>
 							</ul>
 							</div>
 
 				</div>
 				</div>
+				</div>
+				
 				<div className="col-lg-4">
 					<div className="become-teacher__form" id="back-top">
 						<div className="become-teacher__form-top">
@@ -283,9 +283,118 @@ const Trunghoc_details = () => {
 						</div>
 					</div>
 				</div>
+				<div className="share-post">
+					<span className="share-textt">Chia Sẻ</span>
+					<ul className="flat-social">
+						<li>
+							<a
+								xlms="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+								href="http://www.facebook.com/sharer.php?u=https://newgoldenroad.com/?p=42100"
+								className="social-facebook"
+								rel="external"
+								target="_blank"
+							>
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									width="16"
+									height="16"
+									fill="currentColor"
+									class="bi bi-facebook"
+									viewBox="0 0 16 16"
+								>
+									<path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
+								</svg>
+								{''}
+								<span>Facebook</span>
+							</a>
+						</li>
+						<li>
+							<a
+								href="https://twitter.com/intent/tweet?text=Tr%C6%B0%E1%BB%9Dng+Alberta+University+%E2%80%93+Du+h%E1%BB%8Dc+Canada+%E2%80%93+Du+h%E1%BB%8Dc+T%C3%A2n+Con+%C4%90%C6%B0%E1%BB%9Dng+V%C3%A0ng&url=https://newgoldenroad.com/?p=42100"
+								className="social-twitter"
+								rel="external"
+								target="_blank"
+							>
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									width="16"
+									height="16"
+									fill="currentColor"
+									class="bi bi-twitter"
+									viewBox="0 0 16 16"
+								>
+									<path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z" />
+								</svg>{' '}
+								<span>Twitter</span>
+							</a>
+						</li>
+						<li>
+							<a
+								href="https://plusone.google.com/_/+1/confirm?hl=en&url=https://newgoldenroad.com/?p=42100&name=Tr%C6%B0%E1%BB%9Dng+Alberta+University+%E2%80%93+Du+h%E1%BB%8Dc+Canada+%E2%80%93+Du+h%E1%BB%8Dc+T%C3%A2n+Con+%C4%90%C6%B0%E1%BB%9Dng+V%C3%A0ng"
+								className="social-google-plus"
+								rel="external"
+								target="_blank"
+							>
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									width="16"
+									height="16"
+									fill="currentColor"
+									class="bi bi-google"
+									viewBox="0 0 16 16"
+								>
+									<path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z" />
+								</svg>{' '}
+								<span>Google +</span>
+							</a>
+						</li>
+					
+						<li>
+							<a
+								href="http://www.linkedin.com/shareArticle?mini=true&url=https://newgoldenroad.com/?p=42100&title=Tr%C6%B0%E1%BB%9Dng+Alberta+University+%E2%80%93+Du+h%E1%BB%8Dc+Canada+%E2%80%93+Du+h%E1%BB%8Dc+T%C3%A2n+Con+%C4%90%C6%B0%E1%BB%9Dng+V%C3%A0ng"
+								className="social-linkedin"
+								rel="external"
+								target="_blank"
+							>
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									width="16"
+									height="16"
+									fill="currentColor"
+									class="bi bi-linkedin"
+									viewBox="0 0 16 16"
+								>
+									<path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z" />
+								</svg>{' '}
+								<span>LinkedIn</span>
+							</a>
+						</li>
+						<li>
+							<a
+								href="http://pinterest.com/pin/create/button/?url=https://newgoldenroad.com/?p=42100&description=Tr%C6%B0%E1%BB%9Dng+Alberta+University+%E2%80%93+Du+h%E1%BB%8Dc+Canada+%E2%80%93+Du+h%E1%BB%8Dc+T%C3%A2n+Con+%C4%90%C6%B0%E1%BB%9Dng+V%C3%A0ng&media=https://newgoldenroad.com/wp-content/uploads/2021/06/du-hoc-tan-con-duong-vang-alberta-university-655x330.png"
+								className="social-pinterest"
+								rel="external"
+								target="_blank"
+							>
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									width="16"
+									height="16"
+									fill="currentColor"
+									class="bi bi-pinterest"
+									viewBox="0 0 16 16"
+								>
+									<path d="M8 0a8 8 0 0 0-2.915 15.452c-.07-.633-.134-1.606.027-2.297.146-.625.938-3.977.938-3.977s-.239-.479-.239-1.187c0-1.113.645-1.943 1.448-1.943.682 0 1.012.512 1.012 1.127 0 .686-.437 1.712-.663 2.663-.188.796.4 1.446 1.185 1.446 1.422 0 2.515-1.5 2.515-3.664 0-1.915-1.377-3.254-3.342-3.254-2.276 0-3.612 1.707-3.612 3.471 0 .688.265 1.425.595 1.826a.24.24 0 0 1 .056.23c-.061.252-.196.796-.222.907-.035.146-.116.177-.268.107-1-.465-1.624-1.926-1.624-3.1 0-2.523 1.834-4.84 5.286-4.84 2.775 0 4.932 1.977 4.932 4.62 0 2.757-1.739 4.976-4.151 4.976-.811 0-1.573-.421-1.834-.919l-.498 1.902c-.181.695-.669 1.566-.995 2.097A8 8 0 1 0 8 0z" />
+								</svg>{' '}
+								<span>Pinterest</span>
+							</a>
+						</li>
+					</ul>
+					<div className="clear" />
+				</div>
 			</div>
-
 		</div>
+
 	);
 };
 export default Trunghoc_details;
