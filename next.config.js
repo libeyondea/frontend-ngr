74ff009b7075e49657eaed8d1429525fc1ff5443
libeyondea <57558120+libeyondea@users.noreply.github.com>
2022-04-20 20:20:32 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,8 +2,6 @@
 const nextConfig = {
 	reactStrictMode: true,
 	eslint: {
-		// Warning: This allows production builds to successfully complete even if
-		// your project has ESLint errors.
 		ignoreDuringBuilds: true
 	}
 };
