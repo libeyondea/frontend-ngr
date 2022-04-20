03a1742126bc991f38af3c8844630441618d8c4b
khanhduy1212 <97206889+khanhduy1212@users.noreply.github.com>
2022-04-20 11:11:34 +0700

    updaotao

--- components/TeachersDetails.js
+++ components/TeachersDetails.js
@@ -1,84 +1,179 @@
 import React from 'react';
 
 const TeachersDetails = () => {
-    return (
-        <section className="team-details">
-            <div className="container">
-                <div className="row justify-content-between">
-                    <div className="col-lg-5">
-                        <div className="team-details__content">
-                            <h2 className="team-details__title">Read my story</h2>
-                            <p className="team-details__text">Lorem Ipsum is simply dummy text of the printing and type
-                                industry.
-                                Lorem Ipsum has been the standard dummy text ever since the when an unknown was
-                                popularised. It
-                                has survived not only five centuries, but also the leap into electronic typesetting
-                                remaining
-                                unchanged.</p>
-                            <h3 className="team-details__subtitle">Certificate</h3>
-                            <ul className="list-unstyled team-details__certificate-list">
-                                <li>
-                                    <img src="/assets/images/certificate-1-1.png" alt="" />
-                                </li>
-                                <li>
-                                    <img src="/assets/images/certificate-1-2.png" alt="" />
-                                </li>
-                            </ul>
-
-                            <div className="progress-one__wrap">
-                                <div className="progress-one__single">
-                                    <div className="progress-one__top">
-                                        <h3 className="progress-one__title">English</h3>
-                                        <h3 className="progress-one__percent"><span className="counter">98</span>%
-                                        </h3>
-                                    </div>
-                                    <div className="progress-one__bar">
-                                        <span style={{width: `98%`}} className="wow slideInLeft"></span>
-                                    </div>
-                                </div>
-                                <div className="progress-one__single">
-                                    <div className="progress-one__top">
-                                        <h3 className="progress-one__title">French</h3>
-                                        <h3 className="progress-one__percent"><span className="counter">64</span>%
-                                        </h3>
-                                    </div>
-                                    <div className="progress-one__bar">
-                                        <span style={{width: `64%`}} className="wow slideInLeft"></span>
-                                    </div>
-                                </div>
-                                <div className="progress-one__single">
-                                    <div className="progress-one__top">
-                                        <h3 className="progress-one__title">Spanish</h3>
-                                        <h3 className="progress-one__percent"><span className="counter">34</span>%</h3>
-                                    </div>
-                                    <div className="progress-one__bar">
-                                        <span style={{width: `34%`}} className="wow slideInLeft"></span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-6">
-                        <div className="team-one__single">
-                            <div className="team-one__image">
-                                <img src="/assets/images/team-d-1.jpg" alt="" />
-                            </div>
-                            <div className="team-one__content">
-                                <h2 className="team-one__name"><a href="team-details.html">Adelaide Hunter</a></h2>
-                                <p className="team-one__designation">Teacher</p>
-                            </div>
-                            <div className="team-one__social">
-                                <a href="#"><i className="fab fa-twitter"></i></a>
-                                <a href="#"><i className="fab fa-facebook-square"></i></a>
-                                <a href="#"><i className="fab fa-pinterest-p"></i></a>
-                                <a href="#"><i className="fab fa-instagram"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+	return (
+		<>
+			<div className="daotao-new">
+	<div>
+  <section className="about-2 section bg-gray" id="z">
+    <div className="container">
+      <div className="row">
+        <div className="col-12 col-md-5">
+          <h2>We have explored the digital landscape with passion for a long time</h2>
+        </div>
+        <div className="col-12 col-md-7">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae deleniti ipsa labore necessitatibus culpa veritatis quo accusantium, neque enim ea ad eaque iure, quas tempore velit, quibusdam dolor illo! Explicabo.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid quisquam maiores iste soluta, nihil dolorem?</p>
+        </div>
+      </div> 		{/* End row */}
+    </div>   	{/* End container */}
+  </section>   {/* End section */}
+  <section className="services section-xs" id="services">
+    <div className="container">
+      <div className="row">
+        {/* Single Service Item */}
+        <div className="col-md-4 col-sm-6 col-xs-12">
+          <div className="service-block color-bg text-center">
+            <div className="service-icon text-center">
+              <i className="tf-ion-ios-copy-outline" />
             </div>
-        </section>
-    );
+            <h3>WordPress Theme</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur.. Sed id lorem eget orci dictum facilisis vel id tellus. Nullam iaculis arcu at mauris dapibus consectetur.</p>
+          </div>
+        </div>
+        {/* End Single Service Item */}
+        {/* Single Service Item */}
+        <div className="col-md-4 col-sm-6 col-xs-12">
+          <div className="service-block text-center">
+            <div className="service-icon text-center">
+              <i className="tf-ion-ios-alarm-outline" />
+            </div>
+            <h3>Responsive Design</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur.. Sed id lorem eget orci dictum facilisis vel id tellus. Nullam iaculis arcu at mauris dapibus consectetur.</p>
+          </div>
+        </div>
+        {/* End Single Service Item */}
+        {/* Single Service Item */}
+        <div className="col-md-4 col-sm-6 col-xs-12 mx-auto">
+          <div className="service-block color-bg text-center">
+            <div className="service-icon text-center">
+              <i className="tf-ion-ios-book-outline" />
+            </div>
+            <h3>Media &amp; Advertisement</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur.. Sed id lorem eget orci dictum facilisis vel id tellus. Nullam iaculis arcu at mauris dapibus consectetur.</p>
+          </div>
+        </div>
+      </div> 		{/* End row */}
+    </div>   	{/* End container */}
+  </section>   {/* End section */}
+</div>
+
+				<section className="about section-sm" id="about">
+					<div className="container">
+						<div className="row mb-50 justify-content-center">
+							<div className="col-md-5 col-sm-12 text-center offset-md-1">
+								<img src="images/about/about-1.jpg" className="img-fluid rounded shadow" alt />
+							</div>
+							<div className="col-md-5 col-sm-12">
+								<div className="content">
+									<h2>
+										Creativity is in our <br /> blood
+									</h2>
+									<p>
+										Curabitur felis nibh, imperdiet eu erat non, luctus cursus lectus. Donec maximus diam leo,
+										sed fringilla nisl auctor eget. Donec dictum neque est, ac faucibus ex blandit a Lorem
+										ipsum dolor sit amet, consectetur adipisicing elit. Recusandae quo minima ab aperiam
+										molestiae natus repellendus neque culpa iure, nemo veritatis explicabo facilis, officia,
+										saepe! Et corrupti odit, non deserunt.
+									</p>
+								</div>
+								<p />
+							</div>
+						</div>{' '}
+						{/* End row */}
+						<div className="row mt-50 justify-content-center">
+							<div className="col-12 col-md-5 offset-md-1 col-md-5 col-sm-12">
+								<div className="content">
+									<h2>
+										We think out of the <br /> Box
+									</h2>
+									<p>
+										Curabitur felis nibh, imperdiet eu erat non, luctus cursus lectus. Donec maximus diam leo,
+										sed fringilla nisl auctor eget. Donec dictum neque est, ac faucibus ex blandit a Lorem
+										ipsum dolor sit amet, consectetur adipisicing elit. Recusandae quo minima ab aperiam
+										molestiae natus repellendus neque culpa iure, nemo veritatis explicabo facilis, officia,
+										saepe! Et corrupti odit, non deserunt.
+									</p>
+								</div>
+								<p />
+							</div>
+							<div className="col-12 col-md-5 col-sm-12 text-center">
+								<img src="images/about/about-2.jpg" className="img-fluid rounded shadow" alt />
+							</div>
+						</div>{' '}
+						{/* End row */}
+					</div>{' '}
+					{/* End container */}
+				</section>
+				<section className="service-2 section bg-gray">
+					<div className="container">
+						<div className="row">
+							<div className="col">
+								<div className="title text-center">
+									<h4>How We Works</h4>
+									<h2>Our work process</h2>
+									<span className="border" />
+									<p>
+										Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum reiciendis quasi itaque,
+										obcaecati atque sit!
+									</p>
+								</div>
+							</div>
+						</div>
+						<div className="row justify-content-center">
+							<div className="col-md-4 col-sm-6 p-0">
+								<div className="service-item text-center">
+									<span className="count">1.</span>
+									<i className="tf-ion-ios-briefcase-outline" />
+									<h4>Project Analysis</h4>
+									<p>
+										Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vitae risus nec dui
+										venenatis.
+									</p>
+								</div>
+							</div>
+							<div className="col-md-4 col-sm-6 p-0">
+								<div className="service-item text-center">
+									<span className="count">2.</span>
+									<i className="tf-ion-ios-alarm-outline" />
+									<h4>Time Management</h4>
+									<p>
+										Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vitae risus nec dui
+										venenatis.
+									</p>
+								</div>
+							</div>
+							<div className="w-100" />
+							<div className="col-md-4 col-sm-6 p-0">
+								<div className="service-item text-center">
+									<span className="count">3.</span>
+									<i className="tf-ion-ios-email-outline" />
+									<h4>Mail Support</h4>
+									<p>
+										Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vitae risus nec dui
+										venenatis.
+									</p>
+								</div>
+							</div>
+							<div className="col-md-4 col-sm-6 p-0">
+								<div className="service-item text-center">
+									<span className="count">4.</span>
+									<i className="tf-ion-ios-locked-outline" />
+									<h4>Secure System</h4>
+									<p>
+										Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vitae risus nec dui
+										venenatis.
+									</p>
+								</div>
+							</div>
+						</div>{' '}
+						{/* End row */}
+					</div>{' '}
+					{/* End container */}
+				</section>
+			</div>
+		</>
+	);
 };
 
 export default TeachersDetails;
