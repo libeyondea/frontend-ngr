9d1ee135df6598bb8b27ddb693d059cbc2bc6376
pt1312 <trangnguyen13122000@gmail.com>
2022-04-21 15:47:01 +0700

    uppp

--- src/pages/dinh_cu_uc.js
+++ src/pages/dinh_cu_uc.js
@@ -5,15 +5,14 @@ import PageHeader from '../components/PageHeader';
 import Footer from '../components/Footer';
 import DinhCuUc from '../components/DinhCuUc';
 
-const TeachersPage = () => {
+const dinh_cu_uc = () => {
 	return (
 		<Layout pageTitle="Tân Con Đường Vàng | Định Cư">
 			<NavOne />
-			<PageHeader title="Định Cư Úc" />
 			<DinhCuUc />
 			<Footer />
 		</Layout>
 	);
 };
 
-export default TeachersPage;
+export default dinh_cu_uc;
