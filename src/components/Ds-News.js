ccc3061bdae2faeb615691ee0febdb59fb3c739a
hieuhunter <84726273+hieuhunter@users.noreply.github.com>
2022-04-22 16:23:28 +0700

    upupupuppupupupu
    dddd

--- src/components/Ds-News.js
+++ src/components/Ds-News.js
@@ -10,7 +10,7 @@ const DS_NEWS = ({ post }) => {
 							alt=""
 						/>
 
-						<a className="blog-one__plus" href="/news-details">
+						<a className="blog-one__plus" href={`/p/${post.translations[0].slug}`}>
 							<i className="kipso-icon-plus-symbol"></i>
 						</a>
 					</div>
@@ -33,10 +33,10 @@ const DS_NEWS = ({ post }) => {
 							</a>
 						</div>
 						<h2 className="blog-one__title">
-							<a href="/news-details">{post.translations[0].title}</a>
+							<a href={`/p/${post.translations[0].slug}`}>{post.translations[0].title}</a>
 						</h2>
 						<p className="blog-one__text">{post.translations[0].excerpt}</p>
-						<a className="blog-one__link" href="/news-details">
+						<a className="blog-one__link" href={`/p/${post.translations[0].slug}`}>
 							Read More
 						</a>
 					</div>
