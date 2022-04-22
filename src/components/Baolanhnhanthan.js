93e60c5a2271cf31ed1cc7210631ceb95deabe60
pt1312 <trangnguyen13122000@gmail.com>
2022-04-22 10:35:36 +0700

    baolanhnhanthan

--- src/components/Baolanhnhanthan.js
+++ src/components/Baolanhnhanthan.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import Image from './Image';
-const Trunghoc_details = () => {
+const Baolanhnhanthan = () => {
 	return (
-		<div className="THdetails">
-			<section className="inner-banner-THCA">
+		<div >
+			<section className="inner-banner-BL">
 				<div className="container">
-					<h2 className="inner-banner-THCA__title">TRUNG HỌC CANADA</h2>
+					<h2 className="inner-banner-BL__title">BÃO LÃNH THÂN NHÂN</h2>
 				</div>
 			</section>
 			<div id="breaking-news" className="breaking-school" style={{ color: 'color: #E4EAE3' }}>
@@ -35,122 +35,87 @@ const Trunghoc_details = () => {
 				&nbsp;&nbsp;
 				<span typeof="v:Breadcrumb">
 					<a href="https://newgoldenroad.com/chuyen-muc/hoat-dong-cong-ty/" style={{ color: '#81868a' }}>
-						Trung Học Canada
+					ĐỊNH CƯ ÚC
 					</a>
 				</span>
 				&nbsp;&nbsp;
 				<span className="delimiter">/</span>
 				&nbsp;&nbsp;
-				<span className="current">Trường trung học Bodwell High School</span>
+				<span className="current">Định cư Mỹ diện đoàn tụ gia đình</span>
 			</div>
 
 			<div className="row">
 				<div className="col-lg-8">
 					<div className="noidung">
-						<h1 className="tieudeTH">BODWELL HIGH SCHOOL – Vancouver, British Columbia– Du học Tân Con Đường Vàng</h1>
+						<h1 className="tieudeTH">Định cư Mỹ diện đoàn tụ gia đình – Du học Tân Con Đường Vàng</h1>
 
-						<div style={{ color: '#333', fontSize: '20px', fontWeight: 'lighter' }}>
-							<strong>
-								<b>Bodwell High School </b>
-							</strong>
-							là một trường trung học nội trú tư thục được thành lập vào năm 1991, trường tọa lạc tại thành phố đẹp
-							như tranh Vancouver, British Columbia. Bodwell high school được chứng nhận và thanh tra bởi Sở Giáo
-							Dục British Columbia và được công nhận là một trường hàng đầu trong số các trường nội trú Canada.
-						</div>
-				<p style={{textAlign:'center'}}>
-					<img src="/assets/images/Trunghoc/trunghoc1.1.png" alt="canada" className="anhTH" width={'700px'} />
-					</p>
-							
-					
+                                     <form class="row g-3">
+                                <div className="col-md-6">
+                                <label for="inputHolot" class="form-label">Họ, tên lót</label>
+                                    <input type="Holot" class="form-control" id="inputHolot" placeholder="Nhập họ, tên lót"/>
+                                </div>
+                                <div className="col-md-6">
+                                <label for="inputTen" class="form-label">Tên</label>
+                                    <input type="Ten" class="form-control" id="inputTen" placeholder="Nhập tên "/>
+                                </div>
+                                <div className="col-md-6">
+                                    <label for="inputEmailAddress" class="form-label">Email</label>
+                                    <input type="text" class="form-control" id="inputEmail" placeholder="Nhập Email"/>
+                                </div>
+                                <div className="col-md-6">
+                                    <label for="inputSDT" class="form-label">Số điện thoại</label>
+                                    <input type="text" class="form-control" id="inputSDT" placeholder="Nhập số điện thoại"/>
+                                </div>
+                                <div className="col-md-6">
+                                    <div className="form-check">
+                                    <input class="form-check-input" type="checkbox" id="gridCheckNam"/>
+                                    <label class="form-check-label" for="gridCheckNam">
+                                        Nam
+                                    </label>
+                                    </div>
+                                </div>
+                                
+                                <div className="col-12">
+                                    <button type="submit" class="btn btn-primary">ĐĂNG KÝ</button>
+                                </div>
+                                </form>
 
-						<h2 className="mucTH">CHƯƠNG TRÌNH ĐÀO TẠO</h2>
-						<div style={{ color: '#333', fontSize: '20px', fontWeight: 'lighter' }}>
-							{' '}
-							Cử nhân thương mại Chương trình học của trường Bodwell High School được kiểm tra cẩn thận, có ý nghĩa,
-							đa dạng, cân bằng và quan trọng nhất là tăng cường sự phát triển hài hòa về nhân cách, trí tuệ và thể
-							chất. Trường Bodwell không ngừng đảm bảo sự cải thiện và điều chỉnh chương trình học theo các tiêu
-							chuẩn của Sở Giáo Dục British Columbia.
-							<ul>
-								<li> &#10004; Bậc đào tạo: lớp 8-12</li>
-								<li> &#10004; Chương trình dự bị đại học</li>
-								<li> &#10004; Tiếng anh học thuật và dự bị</li>
-								<li> &#10004; Khóa học hè</li>
-							</ul>
+						<div style={{ color: '#333', fontSize: '20px', fontWeight: 'lighter' , paddingTop:'20px'}}>	
+						Trong 10 năm phát triển lĩnh vực di trú theo diện đầu tư EB-5 tại thị trường Việt Nam, NEW GOLDEN ROAD 
+                        đã nhận được rất nhiều sự quan tâm và gợi ý từ các khách hàng về vấn đề phát triển dịch vụ bảo lãnh thân nhân 
+                        đang sinh sống tại Việt Nam để qua đoàn tụ với gia đình tại Mỹ. Luôn ấp ủ kế hoạch này suốt chặng đường hoạt động 
+                        cùng với việc bám sát phương châm kinh doanh của công ty chính là chúng tôi không chỉ hỗ trợ nhà đầu tư trong quá
+                        trình thực hiện hồ sơ thành công để thực hiện giấc mơ Mỹ mà chúng tôi còn luôn đồng hành cùng các nhà đầu tư để cả 
+                        gia đình cùng an cư và ổn định cuộc sống mới tại Mỹ. Vì vậy, vào đầu tháng 10/2019, NEW GOLDEN ROAD đã mạnh dạn ra mắt 
+                        dịch vụ Bảo Lãnh Nhân Thân (Family Base) cùng với sự động viên khích lệ từ rất nhiều khách hàng và đối tác nhằm thực
+                         hiện nguyện vọng đoàn tụ gia đình, xây dựng gia đình viên mãn, hạnh phúc và thịnh vượng của hầu hết nhà đầu tư Việt
+                          tại Mỹ.
 						</div>
 						<p  style={{textAlign:'center'}}>
-							<img src="/assets/images/Trunghoc/trunghoc1.2.jpeg" alt="canada" className="anhTH" width={'700px'}/>
+							<img src="/assets/images/baolanh/baolanh2.jpg" alt="uc" className="anhTH" width={'700px'}/>
 							</p>
+						<div>
+							<h2 className="mucTH">Các dịch vụ chính của NEW GOLDEN ROAD gồm có:</h2>
+							<div style={{ color: '#333', fontSize: '20px', fontWeight: 'lighter' }}>
+                               <strong> 1. Đối với người có thẻ xanh:</strong>
+								<ul>
+									<li> ● Bảo lãnh vợ/ chồng (khoảng 01 – 02 năm)</li>
+									<li>● Bảo lãnh con độc thân dưới 21 tuổi (khoảng 01 – 02 năm)</li>
+                                    <li>● Bảo lãnh con độc thân trên 21 tuổi (khoảng 06 - 07 năm)</li>
+									</ul>
+                                <strong> 1. Đối với người có quốc tịch:</strong>
+                                    <ul>
+									<li> ● Bảo lãnh vợ/chồng (khoảng 01 năm)</li>
+									<li>● Bảo lãnh cha/ mẹ (khoảng 01 năm)</li>
+									<li>● Bảo lãnh con độc thân dưới 21 tuổi (khoảng 01 năm)</li>
+                                    <li>● Bảo lãnh con độc thân trên 21 tuổi (khoảng 06 – 07 năm)</li>
+                                    <li>● Bảo lãnh con đã có gia đình (khoảng 10 – 11 năm)</li>
+                                    <li>● Bảo lãnh anh chị em (khoảng 11 – 12 năm)</li>
+								
+								</ul>
+							</div>
 						
-						<div>
-							<h2 className="mucTH">ĐIỂM NỔI BẬT CỦA BODWELL HIGH SCHOOL</h2>
-							<div style={{ color: '#333', fontSize: '20px', fontWeight: 'lighter' }}>
-								<ul>
-									<li>
-										{' '}
-										&#10004; Cơ sở vật chất hiện đại với một hồ bơi trong nhà, 2 phòng tập, 2 giảng đường, 2
-										căn tin, các phòng thí nghiệm khoa học, một thư viện và một phòng nhạc.
-									</li>
-									<li>
-										{' '}
-										&#10004; Tỷ lệ học sinh đậu tốt nghiệp hằng năm là 97% và được ghi danh vào các trường Đại
-										học danh tiếng ở Canada.
-									</li>
-									<li>
-										{' '}
-										&#10004; Có đội ngũ tư vấn giúp đỡ cho du học sinh khi gặp vấn đề khó khăn và hướng dẫn
-										chọn trường đại học phù hợp với sở thích của từng du học sinh.
-									</li>
-									<li>
-										{' '}
-										&#10004; Bodwell cấp cho tất cả học sinh laptop được tối ưu cho nghiên cứu học tập, gồm có
-										phần mềm thiết yếu như Microsoft Office 365, Adobe Creative Cloud.
-									</li>
-								</ul>
-							</div>
-							<p  style={{textAlign:'center'}}>
-								<img src="/assets/images/Trunghoc/trunghoc1.3.png" alt="canada" className="anhTH" width={'700px'}/>
-							</p>
-						</div>
-						<div>
-							<h2 className="mucTH">HOẠT ĐỘNG NGOẠI KHÓA</h2>
-							<div style={{ color: '#333', fontSize: '20px', fontWeight: 'lighter' }}>
-								Ngoài việc học tập, Bodwell cũng chú trọng vào tinh thần và thể chất của học sinh. Trường thường
-								tổ chức nhiều sự kiện văn hóa, giải trí:
-								<ul>
-									<li> &#10004; Các chuyến tham quan rạp hát</li>
-									<li> &#10004; Các cuộc thi đấu thể thao</li>
-									<li>
-										{' '}
-										&#10004; Các chuyến dã ngoại đến những thành phố khác nhau ở Canada và tất cả học sinh đều
-										phải tham gia.
-									</li>
-								</ul>
-							</div>
-							<p  style={{textAlign:'center'}}>
-								<img src="/assets/images/Trunghoc/trunghoc1.4.png" alt="canada" className="anhTH" width={'700px'}/>
-							</p>
-						</div>
-						<div>
-							<h2 className="mucTH">KÝ TÚC XÁ</h2>
-							<div style={{ color: '#333', fontSize: '20px', fontWeight: 'lighter' }}>
-								Học sinh sống trong các ký túc xá tại trường sẽ được ở tại các căn phòng (3-4 học sinh) có thể
-								nhìn ra phong cảnh tuyệt đẹp với núi non và vịnh biển. Các nam nữ học sinh sống trong các khu
-								riêng trong ký túc xá dưới sự giám sát 24/24 của các tư vấn viên thanh niên. Ngoài cung cấp dịch
-								vụ nội trú tại trường cho học sinh, Bodwell còn có dịch vụ homestay cho sinh viên quốc tế sống xa
-								nhà.
-							</div>
-							<p  style={{textAlign:'center'}}>
-								<img src="/assets/images/Trunghoc/trunghoc1.5.png" alt="canada" className="anhTH" width={'700px'}/>
-							</p>
-						</div>
-						<div>
-							<h2 className="mucTH">CHI PHÍ</h2>
-							<div style={{ color: '#333', fontSize: '20px', fontWeight: 'lighter' }}>
-								<ul>
-									<li> &#10004; Học phí: $18,000/năm</li>
-									<li> &#10004; Chi phí ăn ở: $16,000/năm</li>
-								</ul>
-							</div>
+				
 							<h3> Mọi chi tiết xin vui lòng liên hệ:</h3>
 						<h5 style={{color:'#ff944d'}}> DU HỌC TÂN CON ĐƯỜNG VÀNG – NEW GOLDEN ROAD</h5>
 						<ol>
@@ -304,8 +269,8 @@ const Trunghoc_details = () => {
 								rel="external noreferrer"
 								target="_blank"
 							>
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
+								<svg style={{marginBottom: '5px',marginRight: '5px'}}
+									xmlns="http://www.w3.org/2000/svg style={{marginBottom: '5px',marginRight: '5px'}}"
 									width="16"
 									height="16"
 									fill="currentColor"
@@ -325,8 +290,8 @@ const Trunghoc_details = () => {
 								rel="external noreferrer"
 								target="_blank"
 							>
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
+								<svg style={{marginBottom: '5px',marginRight: '5px'}}
+									xmlns="http://www.w3.org/2000/svg style={{marginBottom: '5px',marginRight: '5px'}}"
 									width="16"
 									height="16"
 									fill="currentColor"
@@ -334,7 +299,7 @@ const Trunghoc_details = () => {
 									viewBox="0 0 16 16"
 								>
 									<path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z" />
-								</svg>{' '}
+								</svg >{' '}
 								<span>Twitter</span>
 							</a>
 						</li>
@@ -345,8 +310,8 @@ const Trunghoc_details = () => {
 								rel="external noreferrer"
 								target="_blank"
 							>
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
+								<svg style={{marginBottom: '5px',marginRight: '5px'}}
+									xmlns="http://www.w3.org/2000/svg style={{marginBottom: '5px',marginRight: '5px'}}"
 									width="16"
 									height="16"
 									fill="currentColor"
@@ -354,7 +319,7 @@ const Trunghoc_details = () => {
 									viewBox="0 0 16 16"
 								>
 									<path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z" />
-								</svg>{' '}
+								</svg >{' '}
 								<span>Google +</span>
 							</a>
 						</li>
@@ -366,8 +331,8 @@ const Trunghoc_details = () => {
 								rel="external noreferrer"
 								target="_blank"
 							>
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
+								<svg style={{marginBottom: '5px',marginRight: '5px'}}
+									xmlns="http://www.w3.org/2000/svg style={{marginBottom: '5px',marginRight: '5px'}}"
 									width="16"
 									height="16"
 									fill="currentColor"
@@ -375,7 +340,7 @@ const Trunghoc_details = () => {
 									viewBox="0 0 16 16"
 								>
 									<path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z" />
-								</svg>{' '}
+								</svg >{' '}
 								<span>LinkedIn</span>
 							</a>
 						</li>
@@ -386,8 +351,8 @@ const Trunghoc_details = () => {
 								rel="external noreferrer"
 								target="_blank"
 							>
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
+								<svg style={{marginBottom: '5px',marginRight: '5px'}}
+									xmlns="http://www.w3.org/2000/svg style={{marginBottom: '5px',marginRight: '5px'}}"
 									width="16"
 									height="16"
 									fill="currentColor"
@@ -395,7 +360,7 @@ const Trunghoc_details = () => {
 									viewBox="0 0 16 16"
 								>
 									<path d="M8 0a8 8 0 0 0-2.915 15.452c-.07-.633-.134-1.606.027-2.297.146-.625.938-3.977.938-3.977s-.239-.479-.239-1.187c0-1.113.645-1.943 1.448-1.943.682 0 1.012.512 1.012 1.127 0 .686-.437 1.712-.663 2.663-.188.796.4 1.446 1.185 1.446 1.422 0 2.515-1.5 2.515-3.664 0-1.915-1.377-3.254-3.342-3.254-2.276 0-3.612 1.707-3.612 3.471 0 .688.265 1.425.595 1.826a.24.24 0 0 1 .056.23c-.061.252-.196.796-.222.907-.035.146-.116.177-.268.107-1-.465-1.624-1.926-1.624-3.1 0-2.523 1.834-4.84 5.286-4.84 2.775 0 4.932 1.977 4.932 4.62 0 2.757-1.739 4.976-4.151 4.976-.811 0-1.573-.421-1.834-.919l-.498 1.902c-.181.695-.669 1.566-.995 2.097A8 8 0 1 0 8 0z" />
-								</svg>{' '}
+								</svg >{' '}
 								<span>Pinterest</span>
 							</a>
 						</li>
@@ -406,4 +371,4 @@ const Trunghoc_details = () => {
 		</div>
 	);
 };
-export default Trunghoc_details;
+export default Baolanhnhanthan;
