06ca5bc88d1273299c5331bb5ca4baccbe108cd5
TramJulie3012 <103029978+TramJulie3012@users.noreply.github.com>
2022-04-25 21:17:27 +0700

    up

--- src/components/DVAnCu.js
+++ src/components/DVAnCu.js
@@ -165,53 +165,46 @@ const DVAnCu = () => {
 						</div>
 					</div>
 					<div className="DoiTac">
-						<div id="carouselExampleCaptions" className="carousel slide" data-ride="carousel">
-							<ol className="carousel-indicators">
-								<li data-target="#carouselExampleCaptions" data-slide-to={0} className="active" />
-								<li data-target="#carouselExampleCaptions" data-slide-to={1} />
-								<li data-target="#carouselExampleCaptions" data-slide-to={2} />
-							</ol>
-							<div className="carousel-inner">
-								<div className="carousel-item active">
-									<img src="..." className="d-block w-100" alt="..." />
-									<div className="carousel-caption d-none d-md-block">
-										<h5>First slide label</h5>
-										<p>Some representative placeholder content for the first slide.</p>
-									</div>
-								</div>
-								<div className="carousel-item">
-									<img src="..." className="d-block w-100" alt="..." />
-									<div className="carousel-caption d-none d-md-block">
-										<h5>Second slide label</h5>
-										<p>Some representative placeholder content for the second slide.</p>
-									</div>
-								</div>
-								<div className="carousel-item">
-									<img src="..." className="d-block w-100" alt="..." />
-									<div className="carousel-caption d-none d-md-block">
-										<h5>Third slide label</h5>
-										<p>Some representative placeholder content for the third slide.</p>
-									</div>
+						<div className="card-group">
+							<div className="card">
+								<img src="/assets/images/DinhCuMy/" className="card-img-top" alt="..." />
+								<div className="card-body">
+									<h5 className="card-title">Card title</h5>
+									<p className="card-text">
+										This is a wider card with supporting text below as a natural lead-in to additional
+										content. This content is a little bit longer.
+									</p>
+									<p className="card-text">
+										<small className="text-muted">Last updated 3 mins ago</small>
+									</p>
 								</div>
 							</div>
-							<button
-								className="carousel-control-prev"
-								type="button"
-								data-target="#carouselExampleCaptions"
-								data-slide="prev"
-							>
-								<span className="carousel-control-prev-icon" aria-hidden="true" />
-								<span className="sr-only">Previous</span>
-							</button>
-							<button
-								className="carousel-control-next"
-								type="button"
-								data-target="#carouselExampleCaptions"
-								data-slide="next"
-							>
-								<span className="carousel-control-next-icon" aria-hidden="true" />
-								<span className="sr-only">Next</span>
-							</button>
+							<div className="card">
+								<img src="..." className="card-img-top" alt="..." />
+								<div className="card-body">
+									<h5 className="card-title">Card title</h5>
+									<p className="card-text">
+										This card has supporting text below as a natural lead-in to additional content.
+									</p>
+									<p className="card-text">
+										<small className="text-muted">Last updated 3 mins ago</small>
+									</p>
+								</div>
+							</div>
+							<div className="card">
+								<img src="..." className="card-img-top" alt="..." />
+								<div className="card-body">
+									<h5 className="card-title">Card title</h5>
+									<p className="card-text">
+										This is a wider card with supporting text below as a natural lead-in to additional
+										content. This card has even longer content than the first to show that equal height
+										action.
+									</p>
+									<p className="card-text">
+										<small className="text-muted">Last updated 3 mins ago</small>
+									</p>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
