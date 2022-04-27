64c6b38638166238185be6591645c2e7cfb83035
hieuhunter <84726273+hieuhunter@users.noreply.github.com>
2022-04-27 10:17:00 +0700

    uppp111223
    sadasdasd

--- src/pages/doi-tac.js
+++ src/pages/doi-tac.js
@@ -6,17 +6,40 @@ import Footer from '../components/Footer';
 import CallToActionTwo from '../components/CallToActionTwo';
 import Doitac from '../components/Doitac';
 import DTSlide from '../components/Doitac-Slide';
+import http from '../utils/http';
+import pageNumber from '../utils/pageNumber';
 
-const TeachersPage = () => {
+const TeachersPage = ({ posts }) => {
 	return (
 		<Layout pageTitle="Tân Con Đường Vàng | Đối Tác">
 			<NavOne />
 			<DTSlide />
-			<Doitac />
+			<Doitac posts={posts} />
 			<CallToActionTwo />
 			<Footer />
 		</Layout>
 	);
 };
+export async function getServerSideProps({ query }) {
+	try {
+		const resPost = await http.get({
+			url: `/posts`,
+			params: {
+				page: pageNumber(query.page),
+				page_size: 6
+			}
+		});
+		return {
+			props: {
+				posts: resPost.data
+			}
+		};
+	} catch (err) {
+		console.log(err);
+		return {
+			notFound: true
+		};
+	}
+}
 
 export default TeachersPage;
