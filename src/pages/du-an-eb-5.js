53950138469da661a812a303e518e91ebe82742a
pt1312 <trangnguyen13122000@gmail.com>
2022-04-28 10:29:27 +0700

    up

--- src/pages/du-an-eb-5.js
+++ src/pages/du-an-eb-5.js
@@ -4,15 +4,38 @@ import NavOne from '../components/NavOne';
 import PageHeader from '../components/PageHeader';
 import Footer from '../components/Footer';
 import DAEB5 from '../components/DAEB5';
+import http from '../utils/http';
+import pageNumber from '../utils/pageNumber';
 
-const daeb5 = () => {
+const daeb5 = ({posts}) => {
 	return (
 		<Layout pageTitle="Tân Con Đường Vàng | DỰ ÁN ĐẦU TƯ EB-5">
 			<NavOne />		
-			<DAEB5/>
+			<DAEB5 posts={posts} />
 			<Footer />
 		</Layout>
 	);
 };
+export async function getServerSideProps({ query }) {
+	try {
+		const resPost = await http.get({
+			url: `/posts`,
+			params: {
+				page: pageNumber(query.page),
+				page_size: 8
+			}
+		});
+		return {
+			props: {
+				posts: resPost.data
+			}
+		};
+	} catch (err) {
+		console.log(err);
+		return {
+			notFound: true
+		};
+	}
+}
 
 export default daeb5;
