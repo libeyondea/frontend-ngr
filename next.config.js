609ff7a05fe721dbf132faf34c3dcb3dab461b5d
Anh Đây Mấy Đứa <103025377+letuananh99@users.noreply.github.com>
2022-04-29 15:53:02 +0700

    up

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true
+	reactStrictMode: true.valueOf,
+	async redirects() {
+		return [
+		  {
+			source: '/tim-hieu-ve-canada',
+			destination: '/thong-tin-nuoc-canada',
+			permanent: true,
+		  },
+		]
+	  },
 };
 
 module.exports = nextConfig;
