ccae553596943eb34549bbab809fb2b3c8bf7eb6
Anh Đây Mấy Đứa <103025377+letuananh99@users.noreply.github.com>
2022-05-06 09:40:28 +0700

    up

--- src/pages/category/thong-tin-nuoc-canada.js
+++ src/pages/category/thong-tin-nuoc-canada.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import Layout from "../components/Layout";
-import NavOne from "../components/NavOne";
-import PageHeader from "../components/PageHeader";
-import Footer from "../components/Footer";
-import ThongTinCa from "../components/ThongTinCa";
+import Layout from "../../components/Layout";
+import NavOne from "../../components/NavOne";
+import PageHeader from "../../components/PageHeader";
+import Footer from "../../components/Footer";
+import ThongTinCa from "../../components/ThongTinCa";
 
 const ThongTinCanada = () => {
     return (
