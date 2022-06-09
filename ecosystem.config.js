2446b45d19c5d0ea9a00fac6a3b6b7555e4b754c
libeyondea <57558120+libeyondea@users.noreply.github.com>
2022-06-09 10:39:54 +0700

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,7 @@ module.exports = {
 		{
 			name: 'frontend-ngr',
 			script: 'npm',
+			cwd: '/home/newgoldxgG7/newgoldenroad.edu.vn/public_html',
 			args: 'start',
 			exec_mode: 'cluster',
 			instances: 'max',
@@ -21,8 +22,8 @@ module.exports = {
 			ref: 'origin/main',
 			repo: 'git@github.com:libeyondea/frontend-ngr.git',
 			path: '/home/newgoldxgG7/newgoldenroad.edu.vn/public_html',
-			'post-setup': 'npm install; npm build; npm run setup',
-			'post-deploy': 'npm install; npm build; npm run deploy'
+			'post-setup': 'npm install; npm build; pm2 start ecosystem.config.js --env production',
+			'post-deploy': 'npm install; npm build; pm2 restart ecosystem.config.js --env production'
 		}
 	}
 };
